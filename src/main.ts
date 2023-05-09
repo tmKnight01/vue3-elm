afc01925bbd25993a7e430d3ae490f256e8a794a
tmKnight01 <94892258+tmKnight01@users.noreply.github.com>
2023-05-09 18:11:34 +0800

    上传源代码

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+// import './style.css'
+import './styles/common.scss'
+// import App from './App.vue'
+import Home from '@/pages/Home.vue'
+
+createApp(Home).mount('#app')
