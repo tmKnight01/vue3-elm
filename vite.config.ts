b75b97ec4e7411d5439745978c09ec8903e9aa27
xinshihao <785296341@qq.com>
2023-06-02 17:49:56 +0800

    fix: 修改上线后的路径问题

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VantResolver } from "unplugin-vue-components/resolvers";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'./',
   plugins: [
     vue(),
     Components({
